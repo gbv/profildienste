65b1c3ebf4d03d5e1f8b4702b9f4012c054eec3f
Julius Stropel <julius.stropel@gbv.de>
2019-09-16 07:22:20 +0200

    modified UserGateway

--- src/Profildienst/User/UserGateway.php
+++ src/Profildienst/User/UserGateway.php
@@ -6,5 +6,7 @@ namespace Profildienst\User;
 interface UserGateway{
 
     public function findByID($id);
+    public function findColleagues($id, $isil);
     public function updateUserData($id, $data);
+  
 }
